ed0dce0dfbdcad968f1723bb93d8699da3028683
rakheshprasaath <37951918+rakheshprasaath@users.noreply.github.com>
2018-10-18 12:51:34 +0530

    Add files via upload

---
+++ hms/newuser.php
@@ -0,0 +1,18 @@
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+$con=@mysql_connect("localhost","root","") or die(mysql_error());
+$db=@mysql_select_db("hospital",$con)or die(mysql_error());
+$str="insert into login values('$username','$password')";
+$res=@mysql_query($str)or die(mysql_error());
+if($res>=0)
+{
+echo'<br><br><b>Thank you for registeration !! <br>';
+header("location:home.html");
+}
+
+?>
+<html>
+<br>
+<a href="index.html"><b>Click here to return to the main page</b></a>
+</html>
